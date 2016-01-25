13166a119734f57973fa110714308f6212d7113e
Philipp Bayer <philippbay@gmail.com>
2016-01-25 16:47:26 +0800

    Minor stats added, new reviews added, adds fix to remove goodreads-specific markdown-like links

--- plotWordCloud.py
+++ plotWordCloud.py
@@ -11,6 +11,7 @@ import csv
 
 STOP = stopwords.words("english")
 html_clean = re.compile('<.*?>')
+gr_clean = re.compile('\[.*?\]')
 PRINTABLE = string.printable
 REMOVE = set(["!","(",")",":",".",";",",",'"',"?","-",">","_"])
 
@@ -30,6 +31,8 @@ reader = csv.reader(fh)
 header = reader.next()
 position = header.index('My Review')
 
+reviews = 0
+words = 0
 for ll in reader:
     review = ll[position].lower()
     if not review:
@@ -37,13 +40,17 @@ for ll in reader:
         continue
     # clean strings
     cleaned_review = re.sub(html_clean, '', review)
+    cleaned_review = re.sub(gr_clean, '', cleaned_review)
     cleaned_review = replace_by_space(cleaned_review)
     cleaned_review = filter(lambda x: x in PRINTABLE, cleaned_review)
     # clean words
     cleaned_review = cleaned_review.split()
     cleaned_review = filter(lambda x: x not in STOP, cleaned_review)
+    words += len(cleaned_review)
     all_my_words += cleaned_review
+    reviews += 1
 
+print("You have %s words in %s reviews"%(words, reviews))
 # WordCloud takes only string, no list/set
 wordcloud = WordCloud(max_font_size=200, width=800, height=500).generate(' '.join(all_my_words))
 wordcloud.to_file("GR_wordcloud.png")
