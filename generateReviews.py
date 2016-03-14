aa667eb0693409efd1a1e6cdb5eb61b8061d329f
Philipp Bayer <philippbay@gmail.com>
2016-03-14 16:33:10 +0800

    Changes to directed graph

--- generateReviews.py
+++ generateReviews.py
@@ -15,10 +15,10 @@ mc.dumpdb()
 
 # a key in the datbase looks like:
 # ('when', 'you') defaultdict(<function _one at 0x7f5c843a4500>, 
-# {'just': 0.06250000000059731, 'feel': 0.06250000000059731, 'had': 0.06250000000059731, 'accidentally': 0.06250000000059731, '"love': 0.06250000000059731, 'read': 0.06250000000059731, 'see': 0.06250000000059731, 'base': 0.06250000000059731, 'know': 0.12499999999641617, 'have': 0.12499999999641617, 'were': 0.06250000000059731, 'come': 0.06250000000059731, "can't": 0.06250000000059731, 'are': 0.06250000000059731})
-# so "just" follows after "when you" with 6% probability
+# {'just': 0.06250000000059731, 'feel': 0.06250000000059731, 'had': 0.06250000000059731, 'accidentally': 0.06250000000059731, ''love': 0.06250000000059731, 'read': 0.06250000000059731, 'see': 0.06250000000059731, 'base': 0.06250000000059731, 'know': 0.12499999999641617, 'have': 0.12499999999641617, 'were': 0.06250000000059731, 'come': 0.06250000000059731, 'can't': 0.06250000000059731, 'are': 0.06250000000059731})
+# so 'just' follows after 'when you' with 6% probability
 
-db = pickle.load(open("./markov_db"))
+db = pickle.load(open('./markov_db'))
 # let's get a good node
 #for key in db:
 #    # has in between 5 and 10 connections
@@ -32,7 +32,7 @@ values = db[good_key]
 
 # create the graph
 
-G = nx.Graph()
+G = nx.DiGraph()
 good_key = str(good_key[0])
 G.add_node(good_key)
 G.add_nodes_from(values.keys())
@@ -44,14 +44,14 @@ for partner in values:
     G.add_weighted_edges_from([ (good_key, partner, edge_weight) ])
 
 pos = nx.circular_layout(G)
-nx.draw_networkx_nodes(G, pos, node_color = "white", node_size = 5000)
+nx.draw_networkx_nodes(G, pos, node_color = 'white', node_size = 5000)
 
-# width of edges is based on probability * 50
+# width of edges is based on probability * 10
 for edge in G.edges(data=True):
-    nx.draw_networkx_edges(G, pos, edgelist = [(edge[0], edge[1])], width = edge[2]['weight']*50)
+    nx.draw_networkx_edges(G, pos, edgelist = [(edge[0], edge[1])], width = edge[2]['weight']*10)
 
 nx.draw_networkx_labels(G, pos, font_size=10, font_family='sans-serif')
-plt.axis("off")
+plt.axis('off')
 plt.tight_layout()
-plt.savefig("Markov_graph.png")
+plt.savefig('Markov_graph.png')
 plt.show()
